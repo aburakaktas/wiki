9f428f7e3ce49b4061621bd7b09bd820dcf5e244
Burak Aktas <aburakaktas@gmail.com>
2023-09-17 21:53:21 +0200

    finished entry page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:article_name>", views.article, name="article")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import markdown2
+import re
 from . import util
 
 
@@ -8,3 +10,16 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def article(request, article_name):
+    article = util.get_entry(article_name)
+    if article:
+        article_title = re.findall("^# (.*)", article)
+        # print("title:", article_title[0])
+        # print(article)
+        return render(request, "encyclopedia/article.html",
+                      {"article": markdown2.markdown(article),
+                      "article_title": article_title[0],
+                      })
+    else:
+        return HttpResponse("404 not found")
