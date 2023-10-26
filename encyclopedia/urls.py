fcc1ae3a289d75eddc505161b4bcceae45c9f0b1
Burak Aktas <aburakaktas@gmail.com>
2023-10-26 22:05:17 +0200

    Search function is done

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "wiki"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:article_name>", views.article, name="article"),
-    path("search", views.search, name="search")
+    path("search", views.search, name="search"),
+    path("results", views.results, name="results")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,17 +1,61 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 import markdown2
 import re
 from . import util
+from django import forms
+
+
+class SearchInput(forms.Form):
+    search_query = forms.CharField(label="Search:")
 
 
 def index(request):
+    searchInput = SearchInput()
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form": searchInput,
     })
-    
+
+
 def search(request):
-    return HttpResponse(f"you search {q}")
+    if request.method == "GET":
+        searchInput = SearchInput(request.GET)
+        if searchInput.is_valid():
+            search_query = searchInput.cleaned_data["search_query"]
+
+            # check if search query matches an existing entry. if yes redirect to that page
+            if util.get_entry(search_query):
+                print(search_query, "found")
+                return HttpResponseRedirect(f"/wiki/{search_query}")
+
+            # if entry cannot be found, list entries that match as a substring and redirect to search results page
+            else:
+                print(search_query, "not found")
+                all_entries = util.list_entries()
+                print(all_entries)
+
+                # list all matched entries
+                matched_entries = []
+                for entry in all_entries:
+                    if search_query in entry.lower():
+                        matched_entries.append(entry)
+
+                print("matched entries:", matched_entries)
+                
+                return render(request, "encyclopedia/results.html", {
+                    "matched_entries": matched_entries,
+                    "search_query": search_query
+                })
+
+    return HttpResponseRedirect(reverse("wiki:index"))
+
+
+def results(request):
+    return render(request, "encyclopedia/results.html", {
+
+    })
 
 
 def article(request, article_name):
@@ -22,7 +66,7 @@ def article(request, article_name):
         # print(article)
         return render(request, "encyclopedia/article.html",
                       {"article": markdown2.markdown(article),
-                      "article_title": article_title[0],
-                      })
+                       "article_title": article_title[0],
+                       })
     else:
         return HttpResponse("404 not found")
